897526ae07371604d73f077082d6945eb404ef95
Andrey <nkgrand25@gmail.com>
2020-11-27 00:54:51 +0200

    added li:hover added innerHTML

--- js/order.js
+++ js/order.js
@@ -1,4 +1,9 @@
 (() => {
+  function error() {
+    const modal = document.querySelector("#myModalStaticUnavailable");
+    modal.style.display = "flex";
+  }
+
   function cardClick(wrapper) {
     const el = wrapper.querySelectorAll(".card");
 
@@ -8,10 +13,33 @@
 
     function sliderElClick(button, id) {
       button.addEventListener("click", function () {
-        alert("Added to basket");
+        error();
       });
     }
   }
 
+  function cartClick(wrapper) {
+    const spRequestBtn = wrapper.querySelector("#specialRequests");
+    const orderBtn = wrapper.querySelector("#orderBtn");
+
+    orderBtn.addEventListener("click", function () {
+      error();
+    });
+
+    spRequestBtn.addEventListener("click", function () {
+      // spRequestBtn.style.display = "none";
+      spRequestBtn.innerHTML = `<textarea>Enter</textarea>`;
+      console.log(spRequestBtn);
+      // window.onclick = function (event) {
+      //   if (event.target !== spRequestBtn) {
+      //     spRequestBtn.innerHTML = `<a href="#!" class="comment__link" id="specialRequests"
+      //     >Special requests</a
+      //   >`;
+      //   }
+      // };
+    });
+  }
+
   cardClick(document.querySelector(".card__wrapper"));
+  cartClick(document.querySelector("#orderSide"));
 })();
